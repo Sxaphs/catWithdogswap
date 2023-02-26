1d70f0c4c608215061b3af98185552a61791ba25
vt <vt@MacBook-Pro.local>
2023-02-26 22:37:44 +0100

    update

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -6,7 +6,7 @@ const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
   [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
   [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
-  [ChainId.GÖRLI]: '0x5A1847033379C659C709f06CF9ef52ef0AB31E44' //replace it or add your own chain and mutilcall address.
+  [ChainId.GÖRLI]: '0xD71D981fd5241df8aB14367744756C47aD19d2c5' //replace it or add your own chain and mutilcall address.
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
