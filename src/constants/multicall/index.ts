bd5f12f287ec7215fdb9b809e5d9cdbfb8b0f283
vt <vt@MacBook-Pro.local>
2023-02-26 19:56:43 +0100

    first commit

---
+++ src/constants/multicall/index.ts
@@ -0,0 +1,12 @@
+import { ChainId } from 'catwithdogswap-sdk'
+import MULTICALL_ABI from './abi.json'
+
+const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xeefBa1e63905eF1D7ACbA5a8513c70307C1cE441',
+  [ChainId.ROPSTEN]: '0x53C43764255c17BD724F74c4eF150724AC50a3ed',
+  [ChainId.KOVAN]: '0x2cc8688C5f75E365aaEEb4ea8D6a480405A48D2A',
+  [ChainId.RINKEBY]: '0x42Ad527de7d4e9d9d011aC45B31D8551f8Fe9821',
+  [ChainId.GÖRLI]: '0x5A1847033379C659C709f06CF9ef52ef0AB31E44' //replace it or add your own chain and mutilcall address.
+}
+
+export { MULTICALL_ABI, MULTICALL_NETWORKS }
